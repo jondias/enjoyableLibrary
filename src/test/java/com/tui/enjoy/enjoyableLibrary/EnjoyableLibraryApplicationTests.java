e7f3737de221b76a7f4ee63ede2aedb16f3f1de8
Jonathan Silva <jonathan.silva.e@thalesdigital.io>
2021-12-29 19:14:36 +0000

    Initial setup.

---
+++ src/test/java/com/tui/enjoy/enjoyableLibrary/EnjoyableLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tui.enjoy.enjoyableLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyableLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
