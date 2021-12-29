48026e8c68e979924227f5a3e27bba886c90ec0b
jondias <43037292+jondias@users.noreply.github.com>
2021-12-29 19:20:17 +0000

    Add files via upload

---
+++ src/main/java/com/tui/enjoy/enjoyableLibrary/EnjoyableLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.tui.enjoy.enjoyableLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyableLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyableLibraryApplication.class, args);
+	}
+
+}
